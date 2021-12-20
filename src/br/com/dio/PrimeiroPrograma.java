8c309197b637a8031c4c3fc934a91711d9deb129
Sergio Campos <sermarcia@hotmail.com>
2021-12-20 16:28:56 -0300

    CREATE CLASS LIVRO

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,45 @@ public class PrimeiroPrograma {
         int b = 3;
         System.out.println("Hello World!!!" + (a+b));*/
 
-        Gato gato = new Gato("Chica", "Mesclado", 5);
+//        Gato gato = new Gato("Chica", "Mesclado", 5);
 
-        System.out.println(gato);
+//        System.out.println(gato);
+
+        Livro livro = new Livro("Use a Cabeça jQuery", 310);
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
